ab15561b6efb183cf43be3fd3ef7631ce07ef6b0
«Borisenko <ira-baikonur@yandex.ru>
2023-03-26 18:49:34 +0300

    Задача 8

---
+++ n8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Принять число N и вывести все четные от 1 до N
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int chet = 2;
+
+Console.Write("Четные числа до {0:d}: ", num); 
+
+if (chet > num)  Console.WriteLine(" таких нет");
+
+while (chet <= num)
+{
+    Console.Write(" {0:d},", chet);
+    chet += 2;
+}
