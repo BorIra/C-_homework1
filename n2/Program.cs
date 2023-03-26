00d1e54dae44dcc9901395377ceabcaf1ff70d29
«Borisenko <ira-baikonur@yandex.ru>
2023-03-26 19:02:02 +0300

    Изменения в выводе

--- n2/Program.cs
+++ n2/Program.cs
@@ -5,7 +5,7 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число:");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if(num1 > num2) Console.WriteLine("Число {0:d} больше числа {1:d}", num1, num2);
+if(num1 > num2) Console.WriteLine("Число {0:d} больше", num1);
 else 
-    if (num1 < num2) Console.WriteLine("Число {0:d} меньше числа {1:d}", num1, num2);
+    if (num1 < num2) Console.WriteLine("Число {0:d} больше",num2);
     else Console.WriteLine("Вы ввели два одинаковых числа");
