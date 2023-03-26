b03ac5c9e9ba69e390cf5bae798688168ec3a3bc
«Borisenko <ira-baikonur@yandex.ru>
2023-03-26 18:06:45 +0300

    Задача 4

---
+++ n4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти максимальное тз трех введенных чисел
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (max < num2) max = num2;
+if (max < num3) max = num3;
+
+Console.WriteLine("Максимальное число: {0:d}", max);
