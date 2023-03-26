3944c17648682862a99b17476000c8b678e64c83
«Borisenko <ira-baikonur@yandex.ru>
2023-03-26 18:40:35 +0300

    Задача 6

---
+++ n6/Program.cs
@@ -0,0 +1,6 @@
+﻿// Определить четность числа
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) Console.Write("Число {0:d} четное", num);
+else Console.Write("Число {0:d} нечетное", num);
