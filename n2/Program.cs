659ece49d360d5ec91f669904bffd80461d580b3
«Borisenko <ira-baikonur@yandex.ru>
2023-03-26 19:03:15 +0300

    Задача 2

--- n2/Program.cs
+++ n2/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Сравнение двух введенных чисел
-Console.Write("Введите первое число:");
+Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число:");
+Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if(num1 > num2) Console.WriteLine("Число {0:d} больше", num1);
