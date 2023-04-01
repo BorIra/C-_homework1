7977d82eb8caa89fd59b1a3b6e9b42a887693c6f
«Borisenko <ira-baikonur@yandex.ru>
2023-04-01 16:42:23 +0300

    create n10

---
+++ n10/Program.cs
@@ -0,0 +1,17 @@
+﻿//Вход: трехзначное число
+// Вывод: 2я цифра этого числа
+using System;       // эти используемые директивы
+namespace exp10     // пространство имен
+{
+    class Num2   
+    {
+        static void Main(string[] str) 
+        {
+            Console.Write("Введите трехзначное число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 100 && num < 1000) 
+                Console.Write((num/10)%10);
+            else Console.Write("Введено не трехзначное число");
+        }
+    }
+}
