e48795525da86911f7753c3568f749467e7e3ecc
«Borisenko <ira-baikonur@yandex.ru>
2023-04-01 17:46:01 +0300

    create n13

---
+++ n13/Program.cs
@@ -0,0 +1,24 @@
+﻿//Вход: любое число
+// Вывод: 3я цифра этого числа, либо сообщение об ее отсутствии
+using System;       
+namespace exp13     
+{
+    class Num3   
+    {
+        static void Main(string[] str) 
+        {
+            Console.Write("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num < 100) Console.Write("У введенного числа нет третьей цифры");
+            else 
+            {
+               while (num / 1000 != 0)  
+               {   // укорачиваем число слева до трехзначного
+                   num /= 10;
+               } 
+               Console.Write(num % 10);
+            }
+
+        } 
+    }
+}
