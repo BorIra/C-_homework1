49d59ffe22f26133a8b790b52b2c92a8540d78bd
«Borisenko <ira-baikonur@yandex.ru>
2023-04-01 18:23:59 +0300

    create n23

---
+++ n23/Program.cs
@@ -0,0 +1,21 @@
+﻿//Вход: две точки в трехмерной системе координат
+// Вывод: расстояние между точками
+using System;       // эти используемые директивы
+namespace exp23     // пространство имен
+{
+    class kybi   
+    {
+        static void Main(string[] str) 
+        {
+            Console.Write("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Кубы чисел до {0:d}: ", num);
+
+            for (int i = 1; i <= num; i ++)
+            {
+                Console.Write("{0:d}, ", i*i*i);
+            }
+        }
+    }
+}
