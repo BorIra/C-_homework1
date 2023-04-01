bca25519da02e8cbe4d9488642f0e1ac572e7c14
«Borisenko <ira-baikonur@yandex.ru>
2023-04-01 17:52:49 +0300

    create n15

---
+++ n15/Program.cs
@@ -0,0 +1,17 @@
+﻿//Вход: цифра, обозначающая день недели
+// Вывод: является ли день выходным
+using System;       
+namespace exp15     
+{
+    class weekend   
+    {
+        static void Main(string[] str) 
+        {
+            Console.Write("Введите число: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            if (day < 6) Console.Write("Увы будни");
+            else if (day > 7) Console.Write("Нет такого дня недели");
+            else Console.Write("Ура выходной!");
+        } 
+    }
+}
