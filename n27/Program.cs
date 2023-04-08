e87a571d3f03efaa1752d58aca2d8cea20a756c1
«Borisenko <ira-baikonur@yandex.ru>
2023-04-08 19:31:27 +0300

    create n27

---
+++ n27/Program.cs
@@ -0,0 +1,35 @@
+﻿//Вход: принимает число
+// Вывод: сумма цифр введенного числа
+using System;       
+namespace exp27     
+{
+    class sumDigit   
+    {
+        static void Main(string[] str) 
+        {
+            int setVal()
+            {   // ввод числа
+                Console.Write("Введите число : ");
+                return Convert.ToInt32(Console.ReadLine());
+            }
+            
+            void outputVal(int val, int sum)
+            {   // вывод на экран
+                Console.Write("Сумма цифр числа {0:d}: {1:d}", val, sum);
+            }
+
+            int sumDigit(int digit)
+            {   // сумма цифр числа
+                int sum = 0;
+                for(;digit != 0; digit /= 10)
+                {
+                    sum += digit % 10;
+                }
+                return sum;
+            }
+            
+            int digit = setVal();
+            outputVal(digit, sumDigit(digit));           
+        }
+    }
+}
