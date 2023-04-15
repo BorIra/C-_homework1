a0a3c489e59d3981b2e052d3be7c6c09ac25dafc
«Borisenko <ira-baikonur@yandex.ru>
2023-04-15 18:13:28 +0300

    create n41

---
+++ n41/Program.cs
@@ -0,0 +1,45 @@
+﻿//Вход: числа, которые ввел пользователь
+// Вывод: сколько чисел > 0
+using System;      
+using System.Collections; 
+
+namespace exp41     
+{
+    class PologitNum   
+    {
+        static ArrayList NewCollection()
+        {   
+            ArrayList nums = new ArrayList();
+            string str = "";
+            try 
+            {
+                while (true)
+                {
+                    Console.Write("Введите число (если все числа введены введите символ): ");
+                    str = Console.ReadLine();
+                    if (str != "n")nums.Add(Convert.ToDouble(str));             
+                }
+            }
+            catch (Exception ex) 
+            {
+                
+            }
+            
+            return nums;
+        }
+
+        static int  getNumPologit(ArrayList arr)
+            {   
+                int count = 0;
+                foreach(double a in arr)
+                    if (a > 0) count ++;
+                return count;
+            }
+        
+        static void Main(string[] str) 
+        {
+            ArrayList arrOfNums = NewCollection();
+            Console.WriteLine("Положительных чисел в массиве - {0:d}", getNumPologit(arrOfNums));
+        }
+    }
+}
