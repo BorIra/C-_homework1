574455089c13a0660878b7a739bb0128d5d85760
«Borisenko <ira-baikonur@yandex.ru>
2023-04-16 14:43:26 +0300

    end calc

--- calc/Program.cs
+++ calc/Program.cs
@@ -1,5 +1,5 @@
-﻿//Вход: k1, b1, k2, b2, где каждая пара k и b описывают прямую y=k*x+b
-// Вывод: точка пересечения прямых
+﻿//Вход: строка-выражение для вычисления
+// Вывод: вычислить выражение. Калькулятор.
 using System;       
 namespace calc     
 {
@@ -10,8 +10,9 @@ namespace calc
         {   // определяем допустимые операции и их приоритет
             {'+', 1},
             {'-', 1},
-            {'*', 1},
-            {'/', 1}
+            {'*', 2},
+            {'/', 2}
+          //  {'(', 10}
         };
         static Stack<double> nums = new Stack<double>();  // стек чисел
         static Stack<char> ops = new Stack<char>(); // стек операций и скобок
@@ -28,84 +29,105 @@ namespace calc
             int count = 0;
             for(int i = 0; i < str.Length; i ++)
             {
-                if(str[i]>='0' && str[i]<='9' || str[i]=='.') 
-                {
+                if(str[i]>='0' && str[i]<='9' || str[i]==',') 
+                {   // собираем число из символов
                     tmp += str[i];
                     count++;
                 }
                 else if (prioritetOperation.ContainsKey(str[i]) || str[i] == '(' || str[i] == ')') 
                 {
-                    if (tmp != "") elements.Enqueue(tmp);
-                    elements.Enqueue(str[i].ToString());
+                    if (tmp != "") elements.Enqueue(tmp);   // число в очередь
+                    elements.Enqueue(str[i].ToString());    // знак в очередь
                     tmp = "";
                     count ++;
                 }
                 //else Console.Write("Ошибка ввода");
-               //тут должен быть выход из программы
+                //тут должен быть выход из программы
             }
-            if(tmp != "") elements.Enqueue(tmp);
+            if(tmp != "") elements.Enqueue(tmp);    // последнее число в очередь, если оно есть
             tmp = "";
-           /* foreach (string s in elements)
-            {
-                Console.Write(s + ", ");
-            }*/
         }
 
         static double operation()
-        {
+        {   // выполнение операции над двумя цифрами
             double rez = 0;
-            if (nums.Count<2) return 0;
-            double b = nums.Pop();
+            if (nums.Count<2) return 0; // выход, если цифр в стеке недостаточно
+            double b = nums.Pop();      // извлекаем пару цифр
             double a = nums.Pop();
-            switch (ops.Pop())
+            switch (ops.Pop())          // определяем операцию по знаку
             {
-                case '+' : rez  = a+b; break;
-                case '-' : rez  = a-b; break;
-                case '*' : rez  = a*b; break;
-                case '/' : rez  = a/b; break;
+                case '+' : rez  = a+b; Console.WriteLine("{0} + {1} = {2}", a, b, rez); break;
+                case '-' : rez  = a-b; Console.WriteLine("{0} - {1} = {2}", a, b, rez); break;
+                case '*' : rez  = a*b; Console.WriteLine("{0} * {1} = {2}", a, b, rez); break;
+                case '/' : rez  = a/b; Console.WriteLine("{0} / {1} = {2}", a, b, rez); break;
             }
             return rez;
         }
+
+        //static void 
         static void decision()
-        {            
+        {   // вычисляем все выражение         
             string tmp;
             while(elements.Count > 0)
-            {
-                tmp = elements.Peek();
+            {   // пока в очереди не закончились элементы
+                tmp = elements.Dequeue();   // извлекаем элемент из очереди
+               
                 if (prioritetOperation.ContainsKey(tmp[0]))
                 { // знак
-                  ops.Push(tmp[0]);
+                    while (true)
+                    {   
+                        // разбираемся со скобками и приоритетами предыдущего знака в стеке
+                        int skobka = 0;
+                        if (ops.Count > 0) 
+                        {
+                           if(ops.Peek() == '(') skobka = -10;
+                           else skobka = prioritetOperation[ops.Peek()];
+                        }
+                        // определяем нужно ли проводить операцию над числами, или нет
+                        bool notNeedToCalc = ops.Count == 0 || 
+                                        prioritetOperation[tmp[0]] > skobka;
+                        
+                        if (notNeedToCalc) 
+                        {   
+                            ops.Push(tmp[0]);                            
+                            break;
+                        }  
+                        else nums.Push(operation());                         
+                    }                    
                 }
                 else if (tmp == "(")
-                {
+                {  // открывающую скобку просто добавим в стек
                     ops.Push(tmp[0]);  
                 }
                 else if (tmp == ")")
-                { // скобки
-                    ops.Push(tmp[0]); // temp
+                {   // вычисляем значение в скобках до тех пор, пока не вернемся к открывающей скобке
+                    while(true)
+                    {
+                        if (ops.Peek() == '(') 
+                        {
+                            ops.Pop();
+                            break;
+                        }
+                        nums.Push(operation());                        
+                    }
                 }
                 else 
-                { // число
-                    nums.Push(Convert.ToDouble(tmp));  
+                { // число просто добавим в стек чисел
+                    nums.Push(Convert.ToDouble(tmp));         
                 }
             }
-            if (nums.Count > 0)
-            foreach ( double d in nums)
-            {
-                Console.Write("{0:n}, ", d);
-            }
-            Console.WriteLine();
-            if (ops.Count>0)
-            foreach (char c in ops)
-            {
-                Console.Write("{0}, ", c);
+
+            while(true)
+            {   // выполняем операции над числами, если стек знаков еще не пуст
+                if (ops.Count == 0) break;
+                nums.Push(operation());
             }
         }
 
         static void Main(string[] str) 
         {
             setElements(getString());
-            decision();
+            decision();            
         }
     }
 }
