c963b78d8547ea9ddacebff13bc1ee7ee590a88e
«Borisenko <ira-baikonur@yandex.ru>
2023-04-21 20:38:20 +0300

    add 50/50

--- happy/Game.cs
+++ happy/Game.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 namespace Happy
 {
     class Game
@@ -12,39 +13,49 @@ namespace Happy
 
         public Game()
         {
-            for(int i = 0; i < 3; i ++)
-              questions.Add(new Question(new List<string> {
-                "Что растёт в огороде?",
-                "Лук",
-                "Пистолет",
-                "Пулемёт", 
-                "Ракета" 
-              }, 1));
-
+            StreamReader sr = new StreamReader("FileQuestions", System.Text.Encoding.Default);
+            while (sr.EndOfStream != true)
+            {
+                questions.Add(new Question(new List<string> {
+                sr.ReadLine(),
+                sr.ReadLine(),
+                sr.ReadLine(),
+                sr.ReadLine(), 
+                sr.ReadLine() 
+              }, Convert.ToInt32(sr.ReadLine()), sr.ReadLine()));
+            }
         }
         
         public void Run()
         {
             foreach(var q in questions)
                 {
-                    q.Output();
+                    q.OutputQuestion();
+                    if (fiftyFifty) Console.WriteLine("5. Вы можете воспользоваться подсказкой 50/50!");
                     int ans = int.Parse(Console.ReadLine());
+                    if (ans == 5) 
+                    {
+                        fiftyFifty = false;
+                        q.OutputFifthyFifthy();
+                        ans = int.Parse(Console.ReadLine());
+                    }
                     if(ans == q.TrueAns) 
                     {
                         ind++;
                         coins += (int) Math.Pow(2, ind);
-                        Console.WriteLine("Вы правы! У вас {0} очков!", coins);
+                        Console.WriteLine($"Вы правы! {q.comment}. У вас {coins} очков!");
+                        //Console.WriteLine("Вы правы! У вас {0} очков!", coins);
                     }
                     else
                     {
                         if(life) 
                         {
                             life = false;
-                            Console.WriteLine("Вы не правы! У вас {0} очков!", coins);
+                            Console.WriteLine($"Вы не правы! {q.comment}. У вас {coins} очков!");
                         }
                         else 
                         {
-                            Console.WriteLine("Игра окончена! У вас {0} очков!", coins);
+                            Console.WriteLine($"Вы не правы! {q.comment}. Игра окончена! У вас {coins} очков!");
                             break;
                         }
                     }
--- happy/Question.cs
+++ happy/Question.cs
@@ -7,19 +7,45 @@ namespace Happy
         public string Quest;
         public List <string> Ans;
         public int TrueAns;
+        public string comment;
 
-        public Question(List<string> str, int answer)
+        public Question(List<string> str, int answer, string comentToAnswer)
         {
             Quest = str[0];
             Ans = str.Skip(1).ToList(); 
             TrueAns = answer;       
+            comment = comentToAnswer;
         }
 
-        public void Output()
+        public void OutputQuestion()
         {
             Console.WriteLine(Quest);
             for(int i = 0; i < 4; i++)
                 Console.WriteLine("{0}. {1}", i+1, Ans[i]);
+        }
+
+        public void OutputFifthyFifthy()
+        {
+            Random rand = new Random();
+            int falseVariant;
+            do 
+            {
+                falseVariant = rand.Next(0, 4);
+                //Console.WriteLine($"falseVariant={falseVariant}, ans ={TrueAns}");
+            }
+            while ((falseVariant+1) == TrueAns);
+            
+            if (falseVariant < TrueAns)
+            {
+                Console.WriteLine($"{falseVariant+1}. { Ans[falseVariant]}");
+                Console.WriteLine($"{TrueAns}. { Ans[TrueAns-1]}");
+            }
+            else
+            {
+                Console.WriteLine($"{TrueAns}. { Ans[TrueAns-1]}");
+                Console.WriteLine($"{falseVariant+1}. { Ans[falseVariant]}");
+            }
+
         }
      
     }
